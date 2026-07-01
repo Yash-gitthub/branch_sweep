7d8a693f1b934c4efa3e4d0896fd07d2c2ba361b
Yash-gitthub <yashbodake8@gmail.com>
2026-07-01 18:14:03 +0530

    improvement in storing branch names

--- branch_sweep.py
+++ branch_sweep.py
@@ -2,6 +2,11 @@ import subprocess
 def main():
     print("branch-sweep starting ...")
     result = subprocess.run(["git","branch","--merged","master"], capture_output=True, text=True)
-    print(result.stdout)
+    r = result.stdout.splitlines()
+    for i in range(len(r)):
+        r[i] = r[i].strip()
+        if r[i].startswith("* "):
+            r[i] = r[i][2:]
+    print(r)
 if __name__ == "__main__":
     main()
