90c840c0fc735791e77b5373ffbda678aea212da
Yash-gitthub <yashbodake8@gmail.com>
2026-07-01 18:18:32 +0530

    created a protection list

--- branch_sweep.py
+++ branch_sweep.py
@@ -2,11 +2,19 @@ import subprocess
 def main():
     print("branch-sweep starting ...")
     result = subprocess.run(["git","branch","--merged","master"], capture_output=True, text=True)
+    non_deletable = []
+    current_branch = None
     r = result.stdout.splitlines()
     for i in range(len(r)):
         r[i] = r[i].strip()
         if r[i].startswith("* "):
             r[i] = r[i][2:]
+            current_branch = r[i]
+    non_deletable.append(current_branch)
+    if "master" not in non_deletable:
+        non_deletable.append("master")
+    if "main" not in non_deletable:
+        non_deletable.append("main")
     print(r)
 if __name__ == "__main__":
     main()
