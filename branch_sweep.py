34b5fa91816307f95da03ecd97a949db38d038cc
Yash-gitthub <yashbodake8@gmail.com>
2026-07-01 18:20:37 +0530

    refactored protection list logic

--- branch_sweep.py
+++ branch_sweep.py
@@ -2,7 +2,7 @@ import subprocess
 def main():
     print("branch-sweep starting ...")
     result = subprocess.run(["git","branch","--merged","master"], capture_output=True, text=True)
-    non_deletable = []
+    protected = []
     current_branch = None
     r = result.stdout.splitlines()
     for i in range(len(r)):
@@ -10,11 +10,8 @@ def main():
         if r[i].startswith("* "):
             r[i] = r[i][2:]
             current_branch = r[i]
-    non_deletable.append(current_branch)
-    if "master" not in non_deletable:
-        non_deletable.append("master")
-    if "main" not in non_deletable:
-        non_deletable.append("main")
+    protected = [current_branch, "master", "main"]
+    print(protected)
     print(r)
 if __name__ == "__main__":
     main()
