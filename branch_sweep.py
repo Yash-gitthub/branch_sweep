5ab8734706e2be52fe63eaa9f84554f68754b918
Yash-gitthub <yashbodake8@gmail.com>
2026-07-01 18:28:47 +0530

    added filtering of deletable lists

--- branch_sweep.py
+++ branch_sweep.py
@@ -3,6 +3,7 @@ def main():
     print("branch-sweep starting ...")
     result = subprocess.run(["git","branch","--merged","master"], capture_output=True, text=True)
     protected = []
+    deletable = []
     current_branch = None
     r = result.stdout.splitlines()
     for i in range(len(r)):
@@ -11,7 +12,11 @@ def main():
             r[i] = r[i][2:]
             current_branch = r[i]
     protected = [current_branch, "master", "main"]
-    print(protected)
+    for branch in r:
+        if branch not in protected:
+            deletable.append(branch)
     print(r)
+    print(protected)
+    print(deletable)
 if __name__ == "__main__":
     main()
